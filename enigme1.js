0ab12b28e37788d410e5519fa1c55a7334d74fe5
Adrian-Castro-Untoria <71886154+Adrian-Castro-Untoria@users.noreply.github.com>
2023-11-25 13:40:25 -0500

    Add files via upload

---
+++ enigme1.js
@@ -0,0 +1,18 @@
+<script>
+
+var iframe=document.getElementById("iFrame");
+function b1() {
+    document.getElementById('image')
+                .style.display = "block";
+            
+                document.getElementById('b2')
+                .style.display = "block";
+}
+function b2() {
+
+document.getElementById('image')
+                .style.display = "none";
+ document.getElementById('b2')
+                .style.display = "none";
+}
+</script>
